52b71171a420dae46f03b87261f625f266455642
Paulo Ricardo M <pr741234@gmail.com>
2023-08-10 14:06:34 -0300

    criado nova estrutura

---
+++ public/questions.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    question:
+      "Qual é o nome da subordem que inclui as baleias verdadeiras ou baleias de barbatana?",
+    answers: [
+      { option: "Odontoceti", correct: false, questionLetter: "A" },
+      { option: "Mysticeti", correct: true, questionLetter: "B" },
+      { option: "Cetacea", correct: false, questionLetter: "C" },
+      { option: "Megaptera", correct: false, questionLetter: "D" },
+    ],
+  },
+  {
+    question:
+      "Qual é a espécie de baleia que consegue imitar a voz de um ser humano?",
+    answers: [
+      { option: "Beluga", correct: true, questionLetter: "A" },
+      { option: "Baleia azul", correct: false, questionLetter: "B" },
+      { option: "Baleia jubarte", correct: false, questionLetter: "C" },
+      { option: "Baleia franca", correct: false, questionLetter: "D" },
+    ],
+  },
+  {
+    question:
+      "Qual é o nome do órgão que as baleias usam para produzir sons e se comunicar com outras baleias?",
+    answers: [
+      { option: "Laringe", correct: false, questionLetter: "A" },
+      { option: "Narina", correct: false, questionLetter: "B" },
+      { option: "Órgão de Jacobson", correct: false, questionLetter: "C" },
+      { option: "Órgão de Zuckerkandl", correct: true, questionLetter: "D" },
+    ],
+  },
+  {
+    question: "Qual é a maior baleia do mundo?",
+    answers: [
+      { option: "Baleia azul", correct: true, questionLetter: "A" },
+      { option: "Cachalote", correct: false, questionLetter: "B" },
+      { option: "Orca", correct: false, questionLetter: "C" },
+      { option: "Narval", correct: false, questionLetter: "D" },
+    ],
+  },
+  {
+    question:
+      "Qual é o nome da espécie de baleia que vive em grupos matrifocais, ou seja, centrados na mãe?",
+    answers: [
+      { option: "Orca", correct: true, questionLetter: "A" },
+      { option: "Golfinho", correct: false, questionLetter: "B" },
+      { option: "Beluga", correct: false, questionLetter: "C" },
+      { option: "Boto cor de rosa", correct: false, questionLetter: "D" },
+    ],
+  },
+];
