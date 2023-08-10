82bb5be2459f01072b699a2b47544fc1cbd1bddf
Paulo Ricardo M <pr741234@gmail.com>
2023-08-10 19:00:04 -0300

    atualização script.js

--- public/script.js
+++ public/script.js
@@ -10,23 +10,21 @@ const textFinish = document.querySelector(".finish span");
 const restartButton = document.querySelector(".finish button");
 const tryAgainScreen = document.querySelector(".tryAgainScreen");
 const textTryAgainScreen = document.querySelector(".tryAgainScreen span");
-const tryAgainButton = document.querySelector(".tryAgainScreen button");
+const tryAgainButton = document.querySelector(".tryAgainScreen button");
 
-import questions from "./questions.js"; 
+import questions from "./questions.js";
 
 // Inicializa variáveis para controle do estado do quiz
-
 let currentIndex = 0;
 let questionsCorrect = 0;
 let userName;
 
 // verifica se uma string está vazia ou é numérica
-
 function isStringEmpty(str) {
-    return str.trim().length === 0 || !isNaN(str);
-  }
+  return str.trim().length === 0 || !isNaN(str);
+}
 
-  // Obtém um nome de usuário válido
+// Obtém um nome de usuário válido
 function getValidUserName() {
   do {
     userName = prompt("Bem vindo! Digite aqui o seu nome para começar!");
@@ -94,5 +92,61 @@ function createAnswerButtons(answers) {
     answersContainer.appendChild(answerButton);
   });
 }
- 
- 
+
+function nextQuestion(e) {
+  // Verifica se o elemento clicado é um botão de resposta
+  if (e.target.classList.contains("answer")) {
+    // Verifica se a resposta selecionada está correta
+    if (e.target.getAttribute("data-correct") === "true") {
+      questionsCorrect++;
+    } else {
+      tryAgain();
+    }
+
+    // Verifica se ainda há mais perguntas
+    if (currentIndex < questions.length - 1) {
+      currentIndex++;
+      loadQuestion();
+    } else {
+      finish();
+    }
+  }
+}
+
+// Exibe tela de tente novamente após um erro
+function tryAgain() {
+  textTryAgainScreen.innerHTML = `Humm que pena ${userName}, não foi dessa vez... <br> você acertou ${questionsCorrect} de ${questions.length} questões `;
+  showTryAgainScreen();
+}
+
+// Exibe o resultado final
+function finish() {
+  textFinish.innerHTML = `Parabéns ${userName}!! <br> você acertou ${questionsCorrect} de ${questions.length} questões e conseguiu chegar no final do desafio!`;
+  showFinishScreen();
+}
+
+// Eventos de clique nos botões
+startButton.onclick = () => {
+  getValidUserName();
+  loadQuestion();
+  showQuizScreen();
+};
+
+tryAgainButton.onclick = () => {
+  currentIndex = 0;
+  questionsCorrect = 0;
+  showStartScreen();
+};
+
+restartButton.onclick = () => {
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+  showStartScreen();
+};
+
+// Adiciona evento de clique para todas as respostas
+answersContainer.addEventListener("click", nextQuestion);
+
+// Carregar a tela inicial quando a página carregar
+showStartScreen();
