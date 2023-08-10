d936ad718b9bff0db5da92c318fc357ef1d21863
Geovanna Mickaella <gvmckl@gmail.com>
2023-08-10 18:20:01 -0300

    Atualização script.js

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const startScreen = document.querySelector(".startScreen");
+const startButton = document.getElementById("startButton");
+const quizScreen = document.querySelector(".quizScreen");
+const questionContainer = document.querySelector(".questionContainer");
+const questionsQtd = document.querySelector(".questionsQtd");
+const questionElement = document.querySelector(".question");
+const answersContainer = document.querySelector(".answers");
+const finishContainer = document.querySelector(".finish");
+const textFinish = document.querySelector(".finish span");
+const restartButton = document.querySelector(".finish button");
+const tryAgainScreen = document.querySelector(".tryAgainScreen");
+const textTryAgainScreen = document.querySelector(".tryAgainScreen span");
+const tryAgainButton = document.querySelector(".tryAgainScreen button");
